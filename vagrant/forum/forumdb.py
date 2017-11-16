d3c428743a7a5b5a207b8fc012b9fa55fccb23bc
Mostafa Elsheikh <abdelaleem@gmx.com>
2017-11-17 01:11:58 +0200

    Modifying get_posts() add_posts() using DB-API

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -1,15 +1,27 @@
 # "Database code" for the DB Forum.
 
 import datetime
+import psycopg2
 
 POSTS = [("This is the first post.", datetime.datetime.now())]
-
+DBNAME = "forum"
 def get_posts():
-  """Return all posts from the 'database', most recent first."""
-  return reversed(POSTS)
+    """Return all posts from the 'database', most recent first."""
+    db = psycopg2.connect(database=DBNAME)
+    query = "SELECT content, time FROM posts ORDER BY time DESC"
+    cursor = db.cursor()
+    cursor.execute(query)
+    posts = cursor.fetchall()
+    db.close()
+    return posts
 
 def add_post(content):
-  """Add a post to the 'database' with the current timestamp."""
-  POSTS.append((content, datetime.datetime.now()))
+    """Add a post to the 'database' with the current timestamp."""
+    db = psycopg2.connect(database=DBNAME)
+    query = "INSERT INTO posts (content) VALUES ('{}')".format(content)
+    cursor = db.cursor()
+    cursor.execute(query)
+    db.commit()
+    db.close()
 
 
