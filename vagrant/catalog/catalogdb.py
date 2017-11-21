3c666ae8cc1fc7fcefb856d7fd7f5603d1bde203
Mostafa Elsheikh <abdelaleem@gmx.com>
2017-11-21 12:35:51 +0200

    refactoring code

--- vagrant/catalog/catalogdb.py
+++ vagrant/catalog/catalogdb.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python3
+
 import psycopg2
 
-"""Reporting tool that prints out reports (in plain text) based on the data in the database"""
+"""Reporting tool that prints out reports (in plain text)
+based on the data in the database"""
 
 #  Database name
 DBNAME = 'news'
@@ -11,15 +14,15 @@ DBNAME = 'news'
 #  with the most popular article at the top
 msg1 = 'What are the most popular three articles of all time?'
 query1 = '''
-SELECT 
-  articles.title, 
-  COUNT(articles.title) AS top 
-FROM log 
-JOIN articles 
-  ON log.path = concat('/article/', articles.slug) 
-GROUP  BY articles.title 
-ORDER BY top DESC 
-LIMIT 3
+SELECT title, views
+FROM articles
+INNER JOIN
+    (SELECT path, count(path) AS views
+     FROM log
+     GROUP BY log.path) AS log
+ON log.path = '/article/' || articles.slug
+ORDER BY views DESC
+LIMIT 3;
 '''
 
 #  Who are the most popular article authors of all time?
@@ -28,15 +31,15 @@ LIMIT 3
 #  Present this as a sorted list with the most popular author at the top.
 msg2 = 'Who are the most popular article authors of all time?'
 query2 = '''
-SELECT 
-  authors.name, 
-  COUNT(authors.id) AS top 
-FROM log 
-JOIN articles 
-  ON log.path = concat('/article/', articles.slug) 
-JOIN authors 
-  ON articles.author = authors.id 
-GROUP BY authors.id 
+SELECT
+  authors.name,
+  COUNT(authors.id) AS top
+FROM log
+JOIN articles
+  ON log.path = concat('/article/', articles.slug)
+JOIN authors
+  ON articles.author = authors.id
+GROUP BY authors.id
 ORDER BY top DESC
 '''
 
@@ -48,10 +51,14 @@ query3 = '''
 SELECT date, round(percentage, 3)
 FROM
 (
-    SELECT 
-      time::DATE AS date, 
-      SUM (CASE WHEN status LIKE '%404%' THEN 1 ELSE 0 END)::NUMERIC / COUNT (status) * 100 AS percentage 
-    FROM log 
+    SELECT
+      time::DATE AS date,
+      SUM (
+        CASE WHEN status LIKE '%404%'
+        THEN 1
+        ELSE 0
+        END)::NUMERIC / COUNT (status) * 100 AS percentage
+    FROM log
     GROUP BY date
 )
 AS errors
@@ -61,12 +68,15 @@ WHERE percentage > 1.0;
 
 def get_result_set(query):
     """Connects to DB for fetching result set by executing query"""
-    db = psycopg2.connect(database=DBNAME)
-    cursor = db.cursor()
-    cursor.execute(query)
-    results = cursor.fetchall()
-    db.close()
-    return results
+    try:
+        db = psycopg2.connect(database=DBNAME)
+        cursor = db.cursor()
+        cursor.execute(query)
+        results = cursor.fetchall()
+        db.close()
+        return results
+    except psycopg2.Error as e:
+        print(e.pgerror)
 
 
 def print_result(msg,
@@ -75,26 +85,28 @@ def print_result(msg,
                  show_article=0,
                  show_author=0,
                  show_date=0,
-                 show_popularity=0,
+                 show_pplar=0,
                  show_errors=0):
     """Flexible for printing popular items or error percentage logs"""
     print(msg)  # printing message
-    for index, result in enumerate(results):  # iterating over a fixed list of size(2)
-        print("{}) {}{}{} {}\t{} {}{} {}{}".format(show_index * (index+1),
+    # iterating over a fixed list of size(2)
+    for index, result in enumerate(results, 1):
+        print("{}) {}{}{} {}\t{} {}{} {}{}".format(show_index * index,
                                                    show_article * 'Article:',
                                                    show_author * 'Author:',
                                                    show_date * 'Date:',
                                                    result[0],
-                                                   show_popularity * 'Popularity:',
+                                                   show_pplar * 'Popularity:',
                                                    show_errors * 'Errors:',
                                                    result[1],
-                                                   show_popularity * 'views',
+                                                   show_pplar * 'views',
                                                    show_errors * '%'))
+
 
 if __name__ == '__main__':
     articles = get_result_set(query1)  # loading top articles
-    print_result(msg1, articles, show_article=1, show_popularity=1)
+    print_result(msg1, articles, show_article=1, show_pplar=1)
     authors = get_result_set(query2)  # loading top authors
-    print_result(msg2, authors, show_author=1, show_popularity=1)
+    print_result(msg2, authors, show_author=1, show_pplar=1)
     errors = get_result_set(query3)  # loading errors > 1%
     print_result(msg3, errors, show_date=1, show_errors=1)
